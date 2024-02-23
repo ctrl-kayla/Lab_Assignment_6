829acc6fecfa14dc1698bd4836c53f2b4e99b785
KAYLA BUTLER <ka644072@ucf.edu>
2024-02-22 23:22:37 -0500

    Completed assignment

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,7 +2,18 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	if (high < low) 
+		return -1;
+
+	int mid = (low + high)/2;
+	if (numbers[mid] == value)
+		return mid;
+
+	else if (numbers[mid] > value)
+		return search(numbers, low, mid-1, value);
+	
+	else
+		return search(numbers, mid+1, high, value);
 }
 
 void printArray(int numbers[], int sz)
